14dab6da0d4587b1375bc955eb73e57de222b7a4
Luis Rosario <luisprooc@gmail.com>
2021-10-05 09:43:19 -0400

    feat: add about description

--- theme.config.js
+++ theme.config.js
@@ -3,19 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Luis Rosario.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
+      <time>{YEAR}</time> © Luis Rosario. 🇩🇴
     </small>
   )
 }
